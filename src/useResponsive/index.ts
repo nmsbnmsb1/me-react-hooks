dcb15506021cfa2ec2affd3032fcb1380c830720
nmsbnmsb1 <nmsbnmsb1@126.com>
2025-09-27 18:03:02 +0700

    update pkg

--- src/useResponsive/index.ts
+++ src/useResponsive/index.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { TwScreenSortedList } from './tw-screens';
+
 //
 export * from './tw-screens';
 // Types
@@ -23,8 +24,8 @@ export function handleScreens(ss: Screens): ScreensSortedList {
 // Hook
 export const useResponsive = (ss: Screens | ScreensSortedList = TwScreenSortedList) => {
 	// 对传入的pts根据数据从小到大进行排序
-	let sorted = useMemo(() => (!Array.isArray(ss) ? handleScreens(ss) : ss), [ss]);
-	let getBreakpointState = useCallback(() => {
+	const sorted = useMemo(() => (!Array.isArray(ss) ? handleScreens(ss) : ss), [ss]);
+	const getBreakpointState = useCallback(() => {
 		let clientWidth = 0; // SSR
 		if (document) {
 			// Cross-browser support as described in:
@@ -33,13 +34,13 @@ export const useResponsive = (ss: Screens | ScreensSortedList = TwScreenSortedLi
 			// height = Math.max(document.documentElement!.clientHeight, window.innerHeight || 0);
 		}
 		//
-		let nstate: ScreenState = {} as any;
-		for (let c of sorted) {
+		const nstate: ScreenState = {} as any;
+		for (const c of sorted) {
 			// 记录断点
 			nstate[c.breakpoint] = c;
 			// 计算当前所在的断点
 			if (!nstate.current) {
-				let inRange = clientWidth <= c.maxWidth;
+				const inRange = clientWidth <= c.maxWidth;
 				if (inRange) {
 					nstate.current = c;
 				}
@@ -48,8 +49,8 @@ export const useResponsive = (ss: Screens | ScreensSortedList = TwScreenSortedLi
 		if (!nstate.current) nstate.current = sorted.at(-1)!;
 		//
 		nstate.match = (op: string, bp: string) => {
-			let currentIndex = nstate.current.index;
-			let targetIndex = nstate[bp]?.index;
+			const currentIndex = nstate.current.index;
+			const targetIndex = nstate[bp]?.index;
 			if (targetIndex === undefined) return false;
 			if (op === '<') return currentIndex < targetIndex;
 			if (op === '<=') return currentIndex <= targetIndex;
@@ -61,10 +62,10 @@ export const useResponsive = (ss: Screens | ScreensSortedList = TwScreenSortedLi
 		return nstate;
 	}, [sorted]);
 	//
-	let [state, setState] = useState<ScreenState>(() => getBreakpointState());
-	//biome-ignore lint/correctness/useExhaustiveDependencies:
+	const [state, setState] = useState<ScreenState>(() => getBreakpointState());
+	//biome-ignore lint/correctness/useExhaustiveDependencies: no
 	useEffect(() => {
-		let onResize = () => {
+		const onResize = () => {
 			const now = getBreakpointState();
 			if (now.current.breakpoint !== state.current.breakpoint) {
 				setState(now);
